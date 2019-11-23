4deb21aa955179f3ea2528df797fa1ea17561f9d
OlesiaGuk <olesia.v.guk@yandex.ru>
2019-11-23 17:47:21 +0700

    [jQuery_PhoneBook] Телефонная книга (phoneBookStyle.css сделан до подключения Bootstrap)

---
+++ Tasks10-11_jQuery/src/PhoneBook_jQuery/js/phoneBookScript.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    var surname = $("#surname");
+    var name = $("#name");
+    var phoneNumber = $("#phone-number");
+    var addButton = $("#add-button");
+    var phoneBookNote = $("#phone-book");
+
+    addButton.click(function () {
+
+        phoneBookNote.html()
+    })
+
+})
