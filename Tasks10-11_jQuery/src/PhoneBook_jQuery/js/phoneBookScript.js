8c0d82c5306f3cb4b5b9095e017363e894ffe385
OlesiaGuk <olesia.v.guk@yandex.ru>
2019-11-24 23:16:26 +0700

    [jQuery_PhoneBook] добавлена проверка корректности номера телефона; изменен вывод сообщения валидации формы

--- Tasks10-11_jQuery/src/PhoneBook_jQuery/js/phoneBookScript.js
+++ Tasks10-11_jQuery/src/PhoneBook_jQuery/js/phoneBookScript.js
@@ -5,39 +5,49 @@ $(document).ready(function () {
     var phoneNumber = $("#phone-number");
     var addButton = $("#add-button");
     var phoneBook = $("#phone-book");
+    var formValidationMessage = $(".alert-warning");
+    var numberValidationMessage = $(".alert-danger");
 
     var notesCount = 0;
 
     addButton.click(function () {
-        var validationError = false;
+        if (isNaN(phoneNumber.val())) {
+            numberValidationMessage.removeClass("d-none");
+            return;
+        }
+
+        numberValidationMessage.addClass("d-none");
+
+        var formValidationError = false;
+        formValidationMessage.children().remove();
         inputFields.each(function () {
-            if ($(this).val() === "") {
-                $(this).addClass("is-invalid");
-                $(this).next().show();
-                validationError = true;
+            var self = $(this);
+            if (self.val() === "") {
+                self.addClass("is-invalid");
+                var fieldName = self.parent().prev().html();
+                formValidationMessage.append("<div>\"" + fieldName + "\"");
+                formValidationError = true;
             }
         });
 
-        if (validationError === true) {
+        if (formValidationError === true) {
+            formValidationMessage.removeClass("d-none");
             return;
         }
+
+        formValidationMessage.addClass("d-none");
 
         notesCount++;
         var tr = $("<tr>");
         tr.html("<td></td><td></td><td></td><td></td>" +
             "<td class='text-center'><button type='button' class='btn'>x</button></td>");
 
-        var notesCountText = tr.children().eq(0);
-        var surnameText = tr.children().eq(1);
-        var nameText = tr.children().eq(2);
-        var phoneNumberText = tr.children().eq(3);
+        tr.children().eq(0).text(notesCount);
+        tr.children().eq(1).text(surname.val());
+        tr.children().eq(2).text(name.val());
+        tr.children().eq(3).text(phoneNumber.val());
+
         var deleteButton = tr.children().eq(4);
-
-        notesCountText.text(notesCount);
-        surnameText.text(surname.val());
-        nameText.text(name.val());
-        phoneNumberText.text(phoneNumber.val());
-
         deleteButton.click(function () {
             notesCount--;
             tr.remove();
