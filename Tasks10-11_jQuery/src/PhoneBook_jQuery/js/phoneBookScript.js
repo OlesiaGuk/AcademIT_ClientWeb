7bcf89ddaab893ac8d7c97b7a644ffec083f1695
OlesiaGuk <olesia.v.guk@yandex.ru>
2019-11-24 21:44:27 +0700

    [jQuery_PhoneBook] рабочая версия

--- Tasks10-11_jQuery/src/PhoneBook_jQuery/js/phoneBookScript.js
+++ Tasks10-11_jQuery/src/PhoneBook_jQuery/js/phoneBookScript.js
@@ -1,13 +1,62 @@
 $(document).ready(function () {
+    var inputFields = $(".form-control");
     var surname = $("#surname");
     var name = $("#name");
     var phoneNumber = $("#phone-number");
     var addButton = $("#add-button");
-    var phoneBookNote = $("#phone-book");
+    var phoneBook = $("#phone-book");
+
+    var notesCount = 0;
 
     addButton.click(function () {
+        var validationError = false;
+        inputFields.each(function () {
+            if ($(this).val() === "") {
+                $(this).addClass("is-invalid");
+                $(this).next().show();
+                validationError = true;
+            }
+        });
 
-        phoneBookNote.html()
-    })
+        if (validationError === true) {
+            return;
+        }
 
-})
+        notesCount++;
+        var tr = $("<tr>");
+        tr.html("<td></td><td></td><td></td><td></td>" +
+            "<td class='text-center'><button type='button' class='btn'>x</button></td>");
+
+        var notesCountText = tr.children().eq(0);
+        var surnameText = tr.children().eq(1);
+        var nameText = tr.children().eq(2);
+        var phoneNumberText = tr.children().eq(3);
+        var deleteButton = tr.children().eq(4);
+
+        notesCountText.text(notesCount);
+        surnameText.text(surname.val());
+        nameText.text(name.val());
+        phoneNumberText.text(phoneNumber.val());
+
+        deleteButton.click(function () {
+            notesCount--;
+            tr.remove();
+            updateNumeration();
+        });
+
+        phoneBook.append(tr);
+        inputFields.each(function () {
+            $(this).val("");
+        })
+    });
+
+    inputFields.keyup(function () {
+        $(this).removeClass("is-invalid");
+    });
+
+    function updateNumeration() {
+        $(".table tbody tr").each(function (index) {
+            $(this).find("td:first").text(index + 1);
+        });
+    }
+});
