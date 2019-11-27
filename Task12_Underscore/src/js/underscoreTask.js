0b7e8b76058b2b3a89180773226df5711ad45a74
OlesiaGuk <olesia.v.guk@yandex.ru>
2019-11-27 21:10:45 +0700

    [Underscore] корректировка по замечаниям(1): удалена ссылка на jQuery; обращение к новому полю через точку

--- Task12_Underscore/src/js/underscoreTask.js
+++ Task12_Underscore/src/js/underscoreTask.js
@@ -1,76 +1,75 @@
-$(document).ready(function () {
-    var people = [
-        {
-            name: "Алла",
-            lastName: "Храброва",
-            age: 13
-        },
-        {
-            name: "Виктория",
-            lastName: "Байкова",
-            age: 40
-        },
-        {
-            name: "Сергей",
-            lastName: "Кузнецов",
-            age: 25
-        },
-        {
-            name: "Елена",
-            lastName: "Иванова",
-            age: 68
-        },
-        {
-            name: "Олег",
-            lastName: "Громов",
-            age: 20
-        },
-        {
-            name: "Ирина",
-            lastName: "Варфоломеева",
-            age: 25
-        },
-        {
-            name: "Николай",
-            lastName: "Виноградов",
-            age: 31
-        },
-        {
-            name: "Ольга",
-            lastName: "Томина",
-            age: 35
-        },
-        {
-            name: "Татьяна",
-            lastName: "Федорова",
-            age: 22
-        },
-        {
-            name: "Никита",
-            lastName: "Петров",
-            age: 21
-        }
-    ];
+var people = [
+    {
+        name: "Алла",
+        lastName: "Храброва",
+        age: 13
+    },
+    {
+        name: "Виктория",
+        lastName: "Байкова",
+        age: 40
+    },
+    {
+        name: "Сергей",
+        lastName: "Кузнецов",
+        age: 25
+    },
+    {
+        name: "Елена",
+        lastName: "Иванова",
+        age: 68
+    },
+    {
+        name: "Олег",
+        lastName: "Громов",
+        age: 20
+    },
+    {
+        name: "Ирина",
+        lastName: "Варфоломеева",
+        age: 25
+    },
+    {
+        name: "Николай",
+        lastName: "Виноградов",
+        age: 31
+    },
+    {
+        name: "Ольга",
+        lastName: "Томина",
+        age: 35
+    },
+    {
+        name: "Татьяна",
+        lastName: "Федорова",
+        age: 22
+    },
+    {
+        name: "Никита",
+        lastName: "Петров",
+        age: 21
+    }
+];
 
-    var averageAge = _.reduce(people, function (sum, p) {
-        return sum + p.age;
-    }, 0) / people.length;
+var averageAge = _.reduce(people, function (sum, p) {
+    return sum + p.age;
+}, 0) / people.length;
 
-    console.log("Средний возраст = " + averageAge);
+console.log("Средний возраст = " + averageAge);
 
-    var list = _.chain(people)
-        .filter(function (p) {
-            return p.age >= 20 && p.age <= 30;
-        })
-        .sortBy("age")
-        .value();
+var list = _.chain(people)
+    .filter(function (p) {
+        return p.age >= 20 && p.age <= 30;
+    })
+    .sortBy("age")
+    .value();
 
-    console.log(list);
+console.log(list);
 
-    var addFullName = _.each(people, function (p) {
-        p["fullName"] = p.lastName + " " + p.name;
-    });
-
-    console.log(people);
+_.each(people, function (p) {
+    p.fullName = p.lastName + " " + p.name;
 });
+
+console.log(people);
+
 
