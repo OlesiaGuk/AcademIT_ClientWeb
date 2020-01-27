2ef70b71f8cde6f48acd233d3842953f7a43afc7
OlesiaGuk <olesia.v.guk@yandex.ru>
2020-01-27 15:02:45 +0700

    [Webpack_PhoneBook]

---
+++ Task18_Webpack/public/javascripts/phoneBookService.js
@@ -0,0 +1,17 @@
+define(["ajax"], function (Ajax) {
+    return {
+        addContact: function (contact) {
+            return Ajax.post("/addContact", contact);
+        },
+        deleteContact: function (id) {
+            return Ajax.post("/deleteContact", {id: id});
+        },
+        getContacts: function (term) {
+            var data = {
+                term: term
+            };
+
+            return Ajax.get("/getContacts", data);
+        }
+    };
+});
